783690079f67b33ba88f6ec77f795b4a7a9fdaab
lovesh-ckc <lovesh@lemnyscate.com>
2026-02-18 20:13:40 +0530

    Update next.config.mjs

--- apps/command-center/next.config.mjs
+++ apps/command-center/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@eumetise/ui', '@eumetise/state', '@eumetise/rbac', '@eumetise/security'],
+  transpilePackages: ['@eumetise/ui', '@eumetise/state', '@eumetise/rbac', '@eumetise/security','@eumetise/api'],
 
   typescript: {
     ignoreBuildErrors: true,
